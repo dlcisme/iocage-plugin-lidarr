73d8eedef3129065e1086870996054d085a6f5f3
dlcisme <34501019+dlcisme@users.noreply.github.com>
2021-01-18 11:28:58 -0600

    allow lidarr to be updatable

--- post_install.sh
+++ post_install.sh
@@ -12,6 +12,12 @@ chown -R lidarr:lidarr $DATA_LOCATION
 # give write permission for plugin update
 chmod 755 $DATA_LOCATION
 
+# make lidarr the owner of it's directory
+chown -R lidarr:lidarr /usr/local/share/lidarr
+
+# we need write permission to be able to write plugins update.
+chmod 755 /usr/local/share/lidarr
+
 # enable lidarr to start at boot
 sysrc "lidarr_enable=YES"
 
